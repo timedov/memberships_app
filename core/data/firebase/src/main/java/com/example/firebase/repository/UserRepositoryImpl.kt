3fd5b497d7f9ebd9905e84f4e39ecb2cc45124d4
Timur Medov <fyvakek@gmail.com>
2024-09-10 13:23:56 +0300

    add UserRepository and domain module

---
+++ core/data/firebase/src/main/java/com/example/firebase/repository/UserRepositoryImpl.kt
@@ -0,0 +1,78 @@
+package com.example.firebase.repository
+
+import android.util.Log
+import com.example.domain.model.UserDomainModel
+import com.example.domain.repository.UserRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+import com.example.common.utils.Keys
+import com.example.firebase.mapper.UserDomainModelMapper
+
+class UserRepositoryImpl @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val db: FirebaseFirestore,
+    private val mapper: UserDomainModelMapper
+) : UserRepository {
+
+    override suspend fun signUp(
+        username: String,
+        email: String,
+        password: String
+    ): UserDomainModel {
+//        Log.i("UserRepositoryImpl",
+//            "SignUp(username=$username, email=$email, password=$password)")
+//        val user = createUserWithEmailAndPassword(email, password).copy(username = username)
+//        saveUserToStore(user)
+//        return user
+        TODO("Not yet implemented")
+    }
+
+    //    private suspend fun createUserWithEmailAndPassword(
+//        email: String,
+//        password: String
+//    ): UserDomainModel {
+//        val userFB = auth.createUserWithEmailAndPassword(email, password).await()
+//        return mapper.firebaseUserToUserModel(userFB.user!!)
+//    }
+
+//    private suspend fun saveUserToStore(user: UserDomainModel) {
+//        db.collection(Keys.USERS_COLLECTION_KEY).document(user.uid).set(user).await()
+//    }
+
+    override suspend fun signIn(email: String, password: String): UserDomainModel {
+        Log.i("UserRepositoryImpl", "SignIn(email=$email, password=$password)")
+        val userFB = auth.signInWithEmailAndPassword(email, password).await()
+        return mapper.firebaseUserToUserModel(userFB.user!!)
+    }
+
+
+    override suspend fun getUserById(userId: String): UserDomainModel =
+        mapper.firebaseDocToUserModel(
+            db.collection(Keys.USERS_COLLECTION_KEY).document(userId).get().await()
+        )
+
+    override suspend fun getCurrentUserId(): String? = auth.currentUser?.uid
+
+    override suspend fun getCurrentUserCredentials(): String {
+        val userId = getCurrentUserId()
+        if (userId.isNullOrEmpty()) throw IllegalArgumentException("User not authorized")
+        val documents = db.collection(Keys.USERS_COLLECTION_KEY).document(userId).get().await()
+
+        return documents.get(Keys.USER_USERNAME_KEY) as String
+    }
+
+    override suspend fun updateUserCredentials(username: String): Boolean {
+        val userId = getCurrentUserId() ?: throw IllegalArgumentException("User not authorized")
+
+        db.collection(Keys.USERS_COLLECTION_KEY).document(userId).update(
+            Keys.USER_USERNAME_KEY, username
+        ).await()
+        return true
+    }
+
+    override suspend fun signOut() {
+        auth.signOut()
+    }
+}
---
+++ core/domain/src/main/java/com/example/domain/model/UserDomainModel.kt
@@ -0,0 +1,7 @@
+package com.example.domain.model
+
+data class UserDomainModel(
+    val uid: String,
+    val username: String,
+    val email: String,
+)

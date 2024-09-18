533601b64d4dfd296a8716fb155c1a0be0ec6a95
Timur Medov <fyvakek@gmail.com>
2024-09-18 17:32:11 +0300

    minor changes in domain and ui

---
+++ core/domain/src/main/java/com/example/domain/model/UserDetailsModel.kt
@@ -0,0 +1,9 @@
+package com.example.domain.model
+
+class UserDetailsModel(
+    val username: String = "",
+    val imageUrl: String? = null,
+    val subscribers: String = "",
+    val joinedYear: Int = 0,
+    val about: String = ""
+)

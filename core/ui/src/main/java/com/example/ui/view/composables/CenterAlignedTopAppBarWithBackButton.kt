840ca8a57367e643c2977f81ef8181b6d06762a8
Timur Medov <fyvakek@gmail.com>
2024-09-18 17:32:11 +0300

    minor changes in domain and ui

---
+++ core/ui/src/main/java/com/example/ui/view/composables/CenterAlignedTopAppBarWithBackButton.kt
@@ -0,0 +1,35 @@
+package com.example.ui.view.composables
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.ui.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CenterAlignedTopAppBarWithBackButton(
+    title: String,
+    onBackClick: () -> Unit
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = title,
+                style = MaterialTheme.typography.titleLarge)
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back)
+                )
+            }
+        },
+    )
+}
--- core/ui/src/main/java/com/example/ui/view/composables/LoadingScreen.kt
+++ core/ui/src/main/java/com/example/ui/view/composables/LoadingScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun ShowLoading(isLoading: Boolean) {
+fun LoadingScreen(isLoading: Boolean) {
     if (isLoading) {
         Box(
             modifier = Modifier

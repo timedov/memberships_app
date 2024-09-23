72fba42c4df1e7fcc4b7005bb6e7664bde4d638b
Timur Medov <fyvakek@gmail.com>
2024-09-23 14:50:42 +0300

    add AsyncImageCaching

---
+++ core/ui/src/main/java/com/example/ui/view/composables/AsyncImageCaching.kt
@@ -0,0 +1,38 @@
+package com.example.ui.view.composables
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.imageLoader
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+
+@Composable
+inline fun AsyncImageCaching(
+    model: Any?,
+    contentDescription: String?,
+    imageLoader: ImageLoader = LocalContext.current.imageLoader,
+    @SuppressLint("ModifierParameter") modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+    crossinline builder: ImageRequest.Builder.() -> Unit = {}
+) {
+    val context = LocalContext.current
+    val request = ImageRequest.Builder(context)
+        .data(model)
+        .memoryCachePolicy(CachePolicy.ENABLED)
+        .diskCachePolicy(CachePolicy.ENABLED)
+        .apply(builder)
+        .build()
+
+    AsyncImage(
+        model = request,
+        contentDescription = contentDescription,
+        imageLoader = imageLoader,
+        contentScale = contentScale,
+        modifier = modifier
+    )
+}
